c8c119a9b76f626ac29fc3f5567322a30b9eb9e1
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-07-28 21:15:33 -0300

    Criando serviço para autenticar usuário

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,27 @@
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
+import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
+import { AuthResponse } from 'src/app/models/interfaces/user/auth/AuthResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  signupUser(requestData: SignupUserRequest): Observable<SignupUserResponse> {
+    return this.http.post<SignupUserResponse>(
+      `${this.API_URL}/user`, requestData
+    );
+  }
+
+  authUser(requestData: AuthRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestData);
+  }
+}
