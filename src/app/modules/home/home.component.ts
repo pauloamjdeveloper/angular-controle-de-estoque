1b0c3d5dd5dcd29404a29952c0f54a7459b9155b
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-07-30 20:13:32 -0300

    Aplicando lógica para criar e autenticar usuário

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,28 +1,63 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   loginCard = true;
 
   loginForm = this.formBuilder.group({
     email: ['', Validators.required],
-    password: ['', Validators.required]
+    password: ['', Validators.required],
   });
 
   signupForm = this.formBuilder.group({
     name: ['', Validators.required],
     email: ['', Validators.required],
-    password: ['', Validators.required]
+    password: ['', Validators.required],
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private coockieService: CookieService
+  ) {}
 
-  onSubmitLoginForm(): void {}
+  onSubmitLoginForm(): void {
+    if (this.loginForm.value && this.loginForm.valid) {
+      this.userService.authUser(this.loginForm.value as AuthRequest).subscribe({
+        next: (response) => {
+          if (response) {
+            this.coockieService.set('USER_INFO', response?.token);
+            this.loginForm.reset();
+          }
+        },
+        error: (err) => console.log(err),
+      });
+    }
+  }
 
-  onSubmitSignupForm(): void {}
+  onSubmitSignupForm(): void {
+    if (this.signupForm.value && this.signupForm.valid) {
+      this.userService
+        .signupUser(this.signupForm.value as SignupUserRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              alert('Usuário teste criado com sucesso!');
+              this.signupForm.reset();
+              this.loginCard = true;
+            }
+          },
+          error: (err) => console.log(err),
+        });
+    }
+  }
 }
