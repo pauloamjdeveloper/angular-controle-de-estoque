78b5a909038fa4ff9db395855facb61dca55ce94
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-08-14 13:44:43 -0300

    Criando output para adicionar e editar produto

--- src/app/modules/products/components/products-table/products-table.component.ts
+++ src/app/modules/products/components/products-table/products-table.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { ProductEvent } from 'src/app/models/products/ProductEvent';
 
 @Component({
   selector: 'app-products-table',
@@ -8,6 +10,17 @@ import { GetAllProductsResponse } from 'src/app/models/interfaces/products/respo
 })
 export class ProductsTableComponent {
   @Input() products: Array<GetAllProductsResponse> = [];
+  @Output() productEvent = new EventEmitter<EventAction>();
 
   public productSelected!: GetAllProductsResponse;
+  public addProductEvent = ProductEvent.ADD_PRODUCT_EVENT;
+  public editProductEvent = ProductEvent.EDIT_PRODUCT_EVENT;
+
+  handleProductEvent(action: string, id?: string): void {
+    if (action && action !== '') {
+      const productEventData = id && id !== '' ? { action, id } : { action };
+      this.productEvent.emit(productEventData);
+    }
+  }
+
 }
--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { Subject, takeUntil } from 'rxjs';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { ProductsService } from 'src/app/services/products/products.service';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
@@ -42,7 +43,6 @@ export class ProductsHomeComponent implements OnInit, OnDestroy {
         next: (response) => {
           if (response.length > 0) {
             this.productsDatas = response;
-            console.log('DADOS DE PRODUTOS', this.productsDatas);
           }
         },
         error: (err) => {
@@ -56,6 +56,12 @@ export class ProductsHomeComponent implements OnInit, OnDestroy {
           this.router.navigate(['/dashboard']);
         },
       });
+  }
+
+  handleProductAction(event: EventAction): void {
+    if (event) {
+      console.log('DADOS DO EVENTO RECEBIDO', event);
+    }
   }
 
   ngOnDestroy(): void {
