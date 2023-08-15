222fb15b554b88f9367a8e052f2056336664f5f2
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-08-15 20:12:07 -0300

    Implementando funcionalidade para excluir produto

--- src/app/modules/products/components/products-table/products-table.component.ts
+++ src/app/modules/products/components/products-table/products-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DeleteProductAction } from 'src/app/models/interfaces/products/event/DeleteProductAction';
 import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { ProductEvent } from 'src/app/models/products/ProductEvent';
@@ -11,6 +12,7 @@ import { ProductEvent } from 'src/app/models/products/ProductEvent';
 export class ProductsTableComponent {
   @Input() products: Array<GetAllProductsResponse> = [];
   @Output() productEvent = new EventEmitter<EventAction>();
+  @Output() deleteProductEvent = new EventEmitter<DeleteProductAction>();
 
   public productSelected!: GetAllProductsResponse;
   public addProductEvent = ProductEvent.ADD_PRODUCT_EVENT;
@@ -20,6 +22,14 @@ export class ProductsTableComponent {
     if (action && action !== '') {
       const productEventData = id && id !== '' ? { action, id } : { action };
       this.productEvent.emit(productEventData);
+    }
+  }
+
+  handleDeleteProduct(product_id: string, productName: string): void {
+    if (product_id !== '' && productName !== '') {
+      this.deleteProductEvent.emit({
+        product_id, productName,
+      });
     }
   }
 
--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { Subject, takeUntil } from 'rxjs';
 import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
@@ -20,7 +20,8 @@ export class ProductsHomeComponent implements OnInit, OnDestroy {
     private productsService: ProductsService,
     private productsDtService: ProductsDataTransferService,
     private router: Router,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
   ) {}
 
   ngOnInit(): void {
@@ -61,6 +62,53 @@ export class ProductsHomeComponent implements OnInit, OnDestroy {
   handleProductAction(event: EventAction): void {
     if (event) {
       console.log('DADOS DO EVENTO RECEBIDO', event);
+    }
+  }
+
+  handleDeleteProductAction(event: {
+    product_id: string;
+    productName: string;
+  }): void {
+    if (event) {
+      this.confirmationService.confirm({
+        message: `Confirma a exclusão do produto: ${event?.productName}?`,
+        header: 'Confirmação de exclusão',
+        icon: 'pi pi-exclamation-triangle',
+        acceptLabel: 'Sim',
+        rejectLabel: 'Não',
+        accept: () => this.deleteProduct(event?.product_id),
+      });
+    }
+  }
+
+  deleteProduct(product_id: string) {
+    if (product_id) {
+      this.productsService
+        .deleteProduct(product_id)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Produto removido com sucesso!',
+                life: 2500,
+              });
+
+              this.getAPIProductsDatas();
+            }
+          },
+          error: (err) => {
+            console.log(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao remover produto!',
+              life: 2500,
+            });
+          },
+        });
     }
   }
 
